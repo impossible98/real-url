79e67fbce44c13ceddeb659ea47f998b95619e1f
Chris <42108459+impossible98@users.noreply.github.com>
2021-12-02 23:29:07 +0800

    feat(douyu): init

--- src/cmd/mod.py
+++ src/cmd/mod.py
@@ -1,23 +1,38 @@
 import sys
+import constant.mod
 
 import douyu.mod
 import huya.mod
+
+
+VERSION = constant.mod.VERSION
+
+SHOW_VERSION = constant.mod.APP_NAME + "version: " + VERSION
+
 
 def website():
     website = input("Enter website: ")
 
     if website.startswith("https://www.douyu.com/"):
         # Example: https://www.douyu.com/4044733
+        show_version()
+        print(len(SHOW_VERSION) * "=")
         douyu.mod.main(website)
     elif website.startswith("https://www.huya.com/"):
         # Example: https://www.huya.com/ruozhi
+        show_version()
+        print(len(SHOW_VERSION) * "=")
         huya.mod.main(website)
     else:
         print("Invalid website")
 
 
+def show_version():
+    print(SHOW_VERSION)
+
+
 def main():
     if len(sys.argv) > 1:
-        print("No arguments")
+        show_version()
     else:
         website()
---
+++ src/constant/mod.py
@@ -0,0 +1,2 @@
+APP_NAME = "xStream"
+VERSION = '0.0.1'
